97743c7e42f7ce9d3ce6ae5d59d4dbc1c3d87579
Andrea Kopecna <a.kopecna@seznam.cz>
2018-06-01 08:34:18 +0200

    Setting forms and SQLite database

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+#export FLASK_APP=calendar_app.py
+#export FLASK_DEBUG=1
+
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DASTABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
